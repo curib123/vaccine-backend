a27de94e0bab9cc72037e4d73c9e2e6d459f4262
Aikoiyota <studioanime34@gmail.com>
2025-12-29 14:43:49 +0800

    apply verifiy token

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -9,8 +9,8 @@ import { verifyToken } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
-router.post('/registerUser',verifyToken, registerUser);        // Register user
+router.post('/registerUser', verifyToken, registerUser);        // Register user
 router.post('/loginUser', loginUser);             // Login user
-router.post('/logoutUser',logoutUser);
+router.post('/logoutUser',verifyToken,logoutUser);
 
 export default router;
